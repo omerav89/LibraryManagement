7290d8ef3e26579bc0219014ea2a898b9128a32b
hodnahum <hodnahum@gmail.com>
2021-06-06 20:27:19 +0300

    update screen size

--- app/src/main/java/com/example/librarymanagement/activity/QrScan.java
+++ app/src/main/java/com/example/librarymanagement/activity/QrScan.java
@@ -1,44 +1,22 @@
 package com.example.librarymanagement.activity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.media.ToneGenerator;
-import android.os.Bundle;
-import android.os.Looper;
-import android.os.Parcelable;
-import android.util.Log;
-import android.util.SparseArray;
-import android.view.Gravity;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
-import android.widget.TextView;
-import android.Manifest;
-import android.widget.Toast;
-
-import com.example.librarymanagement.R;
-import com.example.librarymanagement.model.Book;
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
-import com.google.zxing.Result;
-
-import java.io.IOException;
-import java.io.Serializable;
 
 import com.example.librarymanagement.db.DataAccess;
-import com.example.librarymanagement.*;
+import com.example.librarymanagement.model.Book;
+import com.google.zxing.Result;
+
+import java.io.Serializable;
+
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
 
 public class QrScan extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
